5f4849d26a085414b2d65766584ed49e129040f0
santiagoCH14 <santych.4.2@gmail.com>
2022-10-28 12:43:10 -0500

    programnado java

---
+++ Ejercicio05/src/ejercicio5/Ejercicio5.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio5;
+
+/**
+ *
+ * @author UTPL
+ */
+public class Ejercicio5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int var1= 2;
+        int var2= 2;
+        int var3= 12;
+        int var4= 1;
+        int var5= 3;
+        
+        //System.out.println(Math.pow(var1, var2) + var3 - var4) / var5
+        
+        double valor = (Math.pov)(var1, var2)
+    }
+    
+}
